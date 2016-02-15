0cc8053ff8ea98ca465a3db6f12ee67f17aac52d
ajtomato <ajtomato@gmail.com>
2016-02-15 21:53:56 +0800

    Primitive types & wrapper types

--- HelloWorld/src/com/gmail/ajtomato/HelloWorld.java
+++ HelloWorld/src/com/gmail/ajtomato/HelloWorld.java
@@ -1,9 +1,38 @@
 package com.gmail.ajtomato;
 
 public class HelloWorld {
+    
+    private static byte mB;
+    private static short mS;
+    private static int mI;
+    private static long mL;
+    private static float mF;
+    private static double mD;
+    private static boolean mBool;
+    private static char mC;
+
+    private static void primitiveTypes() {
+        System.out.println("There is no unsigned type");
+        System.out.println("byte: " + Byte.MIN_VALUE + " - "+ Byte.MAX_VALUE
+                + ", [Default]: " + mB);
+        System.out.println("short: " + Short.MIN_VALUE + " - "+ Short.MAX_VALUE
+                + ", [Default]: " + mS);
+        System.out.println("int: " + Integer.MIN_VALUE + " - "+ Integer.MAX_VALUE
+                + ", [Default]: " + mI);
+        System.out.println("long: " + Long.MIN_VALUE + " - "+ Long.MAX_VALUE
+                + ", [Default]: " + mL);
+        System.out.println("float: " + Float.MIN_VALUE + " - "+ Float.MAX_VALUE
+                + ", [Default]: " + mF);
+        System.out.println("double: " + Double.MIN_VALUE + " - "+ Double.MAX_VALUE
+                + ", [Default]: " + mD);
+        System.out.println("boolean: " + Boolean.FALSE + " & "+ Boolean.TRUE
+                + ", [Default]: " + mBool);
+        System.out.println("char: " + (int)Character.MIN_VALUE + " - "
+                + (int)Character.MAX_VALUE + ", [Default]: " + (int)mC);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+    	primitiveTypes();
     }
 
 }
